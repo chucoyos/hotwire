7c22ebe48d55ed3f4f353bd10c7103f5028c2ff1
chucoyos <facturasmanzanillo@hotmail.com>
2021-04-20 00:47:55 -0500

    add edit feature

---
+++ app/controllers/completed_controller.rb
@@ -0,0 +1,7 @@
+class CompletedController < ApplicationController
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.completed = !@todo.completed
+        @todo.save
+    end
+end
